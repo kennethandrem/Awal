f8b87409508e04f89db148eed274a2f5a64f2f63
Kenneth Martínez <kennethandremm@gmail.com>
2016-11-11 15:20:10 -0600

    Awal init commit

---
+++ app/src/main/java/com/kennek/awal/Splash.java
@@ -0,0 +1,31 @@
+package com.kennek.awal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+    private static final long SPLASH_SCREEN_DELAY = 1100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, Main.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+
+    }
+}
